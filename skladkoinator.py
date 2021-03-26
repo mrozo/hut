063c81af690c7dd3c9100dad3ed20b2b69f118bd
Krzysztof Mróz <private@mrozo.pl>
2021-03-26 22:37:59 +0100

    Document supported events

--- skladkoinator.py
+++ skladkoinator.py
@@ -99,26 +99,46 @@ class EventHandlers:
 
     @staticmethod
     def nextMonth(event:Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Decrement dues balance for all hackers.
+        Event args: None
+        """
         for due_record in dues.values():
             due_record.balance -= 1
 
     @staticmethod
     def newMember(event:Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Register new member with dues balance = 0
+        Event args: email
+        """
         hacker_email = event.args.pop()
         dues[hacker_email] = HackerDues(datetime.now(), hacker_email)
 
     @staticmethod
     def dueSet(event:Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Set dues balance to value for selected hacker
+        Event args: dues balance, email
+        """
         balance, hacker_email = event.args[0:2]
         dues[hacker_email].balance = Decimal(balance)
 
     @staticmethod
     def dueAdd(event:Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Invrease dues balance for selected hacker by value
+        Event args: value, email
+        """
         balance, hacker_email = event.args[0:2]
         dues[hacker_email].balance += Decimal(balance)
 
     @staticmethod
     def transaction(event: Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Register a financial transaction
+        Event args: amount, subject
+        """
         amount, subject = Decimal(event.args[0]), event.args[1]
         if subject in dues:
             hacker_email = subject
@@ -128,21 +148,37 @@ class EventHandlers:
 
     @staticmethod
     def setDefaultDue(event: Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Set default dues rate
+        Event args: dues rate
+        """
         HouseRules.default_rate = Decimal(event.args[0])
 
     @staticmethod
     def assertHackersExist(event: Event, dues: Mapping[str, HackerDues], rates: HouseRules,
                                    account_balance: AccountState):
+        """
+        Check if list of registered hackers contain provided list of hackers
+        Event args: list of emails
+        """
         hacker_emails = set(event.args)
         assert hacker_emails.issubset(set(dues.keys())), event.comment
 
     @staticmethod
     def assertDefaultDueRateEquals(event: Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Check if default due rate is equal to provided one
+        Event args: expected due rate
+        """
         expected_default_due_rate = Decimal(event.args[0])
         assert HouseRules.default_rate == expected_default_due_rate, event.comment
 
     @staticmethod
     def assertHackerDueBalanceEquals(event: Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Check if dues balance for selected hacker is equal to expected value
+        Event args: email, expected dues balance
+        """
         hacker_email, expected_balance = event.args[0], Decimal(event.args[1])
         actual_balance = dues[hacker_email].balance
         assert actual_balance == expected_balance, f"expected_balance={expected_balance}, " \
@@ -150,6 +186,10 @@ class EventHandlers:
 
     @staticmethod
     def assertHackerDueRateEquals(event: Event, dues: Mapping[str, HackerDues], rates: HouseRules, account_balance: AccountState):
+        """
+        Check if dues rate for selected hacker is equal to expected value
+        Event args: email, expected dues rate
+        """
         hacker_email, expected_due_rate = event.args[0], Decimal(event.args[0])
         assert rates[hacker_email] == expected_due_rate, event.comment
 
